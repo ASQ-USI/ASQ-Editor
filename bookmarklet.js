c5091d5d80b3baf075fd4b7ff3ed6f187df7337a
Vasileios Triglianos <triglian@gmail.com>
2014-02-06 16:45:26 +0100

    points bookmarklet to asq server

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,13 +4,13 @@
     var aeC=document.createElement("link")
     aeC.setAttribute("rel", "stylesheet")
     aeC.setAttribute("type", "text/css")
-    aeC.setAttribute("href", 'https://raw.github.com/ASQ-USI/ASQ-Editor/devel/dist/ASQ-Editor.min.css')
+    aeC.setAttribute("href", 'http://asq.inf.usi.ch/editor/dist/ASQ-Editor.min.css')
     document.getElementsByTagName('head')[0].appendChild(aeC);
 
     //load script
     var aES=document.createElement('SCRIPT');
     aES.type='text/javascript';
-    aES.src='https://raw.github.com/ASQ-USI/ASQ-Editor/devel/dist/ASQ-Editor.min.js';
+    aES.src='http://asq.inf.usi.ch/editor/dist/ASQ-Editor.min.js';
     if(typeof(aES)!=='undefined'){
       document.getElementsByTagName('head')[0].appendChild(aES);
     }
